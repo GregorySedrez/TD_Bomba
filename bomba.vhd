12227fd638f753ff6297a4f483ef1f95fc7ca024
igorbarbosa <igor.barbosa1920@gmail.com>
2017-08-11 19:24:33 -0300

    Implementando maquina de estados

--- bomba.vhd
+++ bomba.vhd
@@ -10,6 +10,11 @@ entity bomba is
 		  CLOCK: in bit; -- CLOCK 1s
 		  CLOCK_NORMAL: in bit; -- CLOCK NORMAL
 		  
+		  EXPLODE: ;
+		  STOP:;
+		  DESARMA:;
+		  SINAL_MUX_CONTADOR:;
+		  
         SAIDA: out bit; -- BIP Explosion
 		  DISPLAY0_contador: out std_logic_vector(6 downto 0); 
 		  DISPLAY1_contador: out std_logic_vector(6 downto 0); 
@@ -19,40 +24,53 @@ end bomba;
 
 type TIPOS_ESTADOS is (estado_0, estado_1, estado_2, estado_3);
 signal estado_atual, proximo_estado: TIPOS_ESTADOS;
+signal REGISTRADOR_0_SAIDA, REGISTRADOR_1_SAIDA: in std_logic_vector(3 downto 0);
 
 architecture arch_bomba of bomba is 
 begin
-	process(CLOCK_NORMAL)
+	
+	REGISTRADOR_0_SAIDA <= SENHA;
+	
+	process(START)
+		-- Faz contador
+	end process;
+	
+	process(estado_atual)
 	begin
-		variable contador_clock: integer;
-		if(clock'event and clock = '1') then
-			contador_clock := contador_clock + 1; 
+		if CLOCK_NORMAL'event and CLOCK_NORMAL = '1' then
+			case estado_atual is
+				when estado_0 => -- Escutar SENHA
+					if(ENTER = '1') then
+						estado_atual <= estado_1; -- Iniciar a bomba
+					end if;
+				when estado_1 => 
+					START = '1';
+					if(ENTER = '1') then -- Escuta a senha para desarmar
+						estado_atual <= 
+					end if;
+			end case;
 		end if;
-		
 	end process;
-
-	process(CLOCK_NORMAL)
-	begin
-		
-		process(RESET) -- ESTADO 0 (SENHA)
-		begin 			
-			estado_atual <= estado_1;
-		end process;
-		
-		process(estado_atual) -- ESTADO 1 (CONTADOR & ENTER SENHA & ESCUTAR FIOS)
-			estado_atual <= estado_2;
-		begin 
-		end process;
-		
-		process(RESET) -- ESTADO 2 (EXPLODIU)
-		begin 
-			estado_atual <= estado_3;
-		end process;
-		
-		process(RESET) -- ESTADO 3 ()
-		begin 
-			estado_atual <= estado_0;
-		end process;
+	
+	-- Comparador
+	
+	
+	
+	process (RESET, CLOCK_NORMAL)
+	begin 
+		estado_atual <= estado_0; -- Primeiro estado
 	end process;
+	
+	process(FIOS)
+	begin 
+		if(FIOS(0) = '0' and FIOS(1) = '1' and FIOS(2) = '1' and FIOS(3) = '1') then
+			SELETORES <= "00"; -- Explodiu
+		if(FIOS(0) = '0' and FIOS(1) = '0' and FIOS(2) = '1' and FIOS(3) = '1') then
+			SELETORES <= "01"; -- Mais rapido
+		if(FIOS(0) = '0' and FIOS(1) = '0' and FIOS(2) = '1' and FIOS(3) = '1') then
+			SELETORES <= "10"; -- Mais lento
+	end process;
+	
+	process()
 	
 end arch_bomba;
